1070ebb934d85b82215b8283822ddc5ccb226f50
Tobydog0501 <tobydog0501@gmail.com>
2022-06-28 10:16:02 +0800

    剩下幹部QAQ

--- commands/rep.js
+++ commands/rep.js
@@ -14,6 +14,7 @@ module.exports = {
             .setTitle('打開回報單！')
             .setDescription('此頻道為： 建議／檢舉／回報／上訴用途頻道\n按下下方的藍色按鈕就可以創建安全的回報單了。')
             .setImage('https://media.discordapp.net/attachments/957177628383641621/978192943976431636/43203ceb2bbffb62.jpg')
-        await msg.channel.send({embeds:[],components:[com]})
+            .setFooter({text:`Powered by Tobydog#4180`,iconURL:msg.author.avatarURL()})
+        await msg.channel.send({embeds:[ebd],components:[com]})
     }
 }
