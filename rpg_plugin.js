fe6a10840b5daee030308109241f2a31b0c159fb
Tobydog0501 <tobydog0501@gmail.com>
2022-06-30 18:28:21 +0800

    rpg_plugin update

--- rpg_plugin.js
+++ rpg_plugin.js
@@ -2,24 +2,25 @@ var ui = require('./env.json');
 const d = new Date()
 module.exports = {
 
-    inital(userId){ //reset
+    async inital(userId){ //reset
         if(!ui[userId]){
             ui[userId] = {'lv':0,'exp':0,'lastMsg':undefined,'tasks':[{'type':undefined,'finish':false},{'type':undefined,'finish':false},{'type':undefined,'finish':false}],'lastTask':undefined}
         }
-        write(ui);
+        await write(ui);
+        return;
     },
 
-    rank(userId){  //read
-        this.inital(userId);
+    async rank(userId){  //read
+        await this.inital(userId);
         return ui[userId];
     },
 
-    exp(msg,Discord){   //earn exp
+    async exp(msg,Discord){   //earn exp
         var totalExp = 0;
-        this.inital(userId);
-        msg.attachments.forEach((v,k)=>{
+        await this.inital(userId);
+        this.checkAttachments(msg).forEach((v,k)=>{
             if(v!==undefined){
-                switch(v.contentType.split('/')[0]){
+                switch(v){
                     case 'image':
                         totalExp += 10;
                         break;
@@ -30,48 +31,71 @@ module.exports = {
             }
         })
         if(d.getMilliseconds()-lastMsg>=10*1000){   //msg exp (1:1)
-            if(msg.content.includes('http')){   //url
+            if(this.checkURL(msg)){   //url
                 totalExp += 10;
             }else{  //only msg
                 //check emoji
                 var ctn = msg.cleanContent
-                while(ctn.includes('<:')){    //len = 18
-                    if(ctn.indexOf(':',ctn.indexOf(':'))-ctn.indexOf('>')===18){
-                        
-                    }
-                }
+                this.checkEmoji(ctn);
             }
         }
         return;
     },
 
-    adminExpSet(bot,userId,msg,Discord){    //give exp
+    async adminExpSet(bot,userId,msg,Discord){    //give exp
         
         return;
     },
 
-    getTask(bot,msg,Discord){  //random task
+    async getTask(bot,msg,Discord){  //random task
         return;
     },
 
-    checkTask(userId){
+    async checkTask(userId){
         return;
     },
 
-    checkAttachments(msg){
-        return;
+    async checkAttachments(msg){
+        return msg.attachments.map((v,k)=>{
+            if(v!==undefined){
+                switch(v.contentType.split('/')[0]){
+                    case 'image':
+                        return 'image';
+                    case 'video':
+                        return 'video';
+                }
+            }
+        })
+    },
+
+    async checkURL(msg){
+        if(msg.content.includes('http')) return true;
+        else return false;
+    },
+
+    async checkEmoji(ctn){
+        while(ctn.includes('<:')){    //len = 18
+            if(ctn.indexOf(':',ctn.indexOf(':'))-ctn.indexOf('>')===18){
+                
+            }
+        }
     },
 
     tasksLists:[]
 
 }
 
-function write(w){
+async function write(w){
     var str = JSON.stringify(w)
     fs.writeFile('./env.json',str,err=>{
-      if(err){
-        console.error(err);
-      }
-      
+        return new Promise((resolve,reject)=>{
+            if(err){
+                reject(`Error: ${err}`)
+              }else{
+                resolve('finished')
+              } 
+            
+        })
+       
     })
 }
