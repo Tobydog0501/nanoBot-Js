43c1503d7d31d674c5db3562eedfff719a05baae
Tobydog0501 <tobydog0501@gmail.com>
2022-07-02 17:51:49 +0800

    Debug: Beta 0.4.7

--- commands/rank.js
+++ commands/rank.js
@@ -14,12 +14,16 @@ module.exports = {
                     .then(async ui=>{
                         await plu.expAmount(userId)
                             .then(async expAmounts=>{
-                                let embed = new Discord.MessageEmbed()
-                                .setTitle('等級查詢')
-                                .setDescription(`目前等級：${ui['lv']}\n經驗值：${ui['exp']}\n總經驗值：${expAmounts}`)   //可能要用%
-                                .setThumbnail(args[0]?msg.guild.members.fetch(args[0]).avatarURL():msg.author.avatarURL()) //被搜尋者頭像
-                                .setColor('RANDOM')
-                            await Emsg.edit({embeds:[embed]})
+                                msg.guild.members.fetch(userId)
+                                    .then(async user=>{
+                                        let embed = new Discord.MessageEmbed()
+                                            .setTitle('等級查詢')
+                                            .setDescription(`目前等級：${ui['lv']}\n經驗值：${ui['exp']}\n總經驗值：${expAmounts}`)   //可能要用%
+                                            .setThumbnail(user.avatarURL()) //被搜尋者頭像
+                                            .setColor('RANDOM')
+                                        await Emsg.edit({embeds:[embed]})
+                                    })
+                                
                             })
                         
                     })
