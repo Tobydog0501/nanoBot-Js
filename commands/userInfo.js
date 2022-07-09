113decc3f6896b65573d6d946f78981529c456b3
Tobydog0501 <tobydog0501@gmail.com>
2022-07-09 20:43:58 +0800

    new launcher

---
+++ commands/userInfo.js
@@ -0,0 +1,8 @@
+module.exports = {
+    name:"userInfo",
+    category:"test",
+    description:"取得使用者資訊",
+    async execute(bot,msg,args,Discord){
+      
+    }
+  }
--- events/client/ready.js
+++ events/client/ready.js
@@ -11,13 +11,7 @@ module.exports = async (Discord,bot) => {
   let guild = await bot.guilds.fetch('926089413933539359');
   await uc(guild);
 
-  setInterval(async()=>{
-    await uc(guild);
-    fetch("https://obvious-cuddly-butterkase.glitch.me",{ signal }).then(console.log('ping')).catch(err=>console.warn(`Hey!`))
-    setTimeout(()=>{
-      controller.abort();
-    },2000)
-  },120000)
+
   
 //   const guild = bot.guilds.cache.get('926089413933539359')
 //   setInterval(()=>{
