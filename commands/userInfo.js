49d0bf6b61134ef512992e04b70b5d8c65e69946
Tobydog0501 <tobydog0501@gmail.com>
2022-07-10 13:14:47 +0800

    keep_alive  ping and uptime command

--- commands/userInfo.js
+++ commands/userInfo.js
@@ -2,7 +2,14 @@ module.exports = {
     name:"userInfo",
     category:"test",
     description:"取得使用者資訊",
+    aliases:['ui','user'],
     async execute(bot,msg,args,Discord){
-      
+      if(!!args[0]){
+        //author ui
+      }else if(args[0].length==18){
+        //searching
+      }else{
+        //not snowflake
+      }
     }
   }
