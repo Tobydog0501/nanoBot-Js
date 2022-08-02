545c03257e29c7cd27ee55e9abad80708b2981a6
Tobydog0501 <tobydog0501@gmail.com>
2022-08-02 19:23:06 +0800

    身分組更新

--- commands/bruh.js
+++ commands/bruh.js
@@ -24,6 +24,7 @@ module.exports = {
           .setOptions([
             { label: '有事請務必叫我~', emoji: "🔊", value: '926270856815071312', description: '我想接收所有伺服器通知!' },
             { label: '重要的事再找我~', emoji: "🔉", value: '926270748564291665', description: '重要的事情不要忘記叫我~' },
+            { label: '機器人更新就找我~', emoji: "🔱", value: '1003984713440055347', description: '機器人的更新我要知道!' },
             { label: '我想淺水水啦!', emoji: "🔇", value: '926270913421377556', description: '不要通知我!很吵!!!' },
             { label: '我歡迎任何人Tag我唷~', emoji: "💫", value: '930797726013202482', description: '我歡迎大家有事來找我唷~' }
           ])
