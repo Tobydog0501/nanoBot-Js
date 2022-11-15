7dbf32cd722a110017286b3555d902f0609ec435
Tobydog0501 <tobydog0501@gmail.com>
2022-11-15 21:24:22 +0800

    Auto mod 搶工作

--- events/guild/messageCreate.js
+++ events/guild/messageCreate.js
@@ -20,27 +20,6 @@ module.exports = async (Discord,bot,msg)=>{
       await msg.reply(`我好像沒有這個指令欸...`)
     }
     return;
-  }else if(/(?=^[^http])(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[^\.]*\.[^\.]*\.[^\.]*[\n]/gm.test(msg.content)){
-    //Discord bot TOKEN
-    let chn = await msg.guild.channels.fetch('1020521950449246268');
-    msg.member.timeout(1000*60*60*24,'傳送TOKEN');
-    let ebd = new Discord.MessageEmbed()
-      .setTitle('警告紀錄')
-      .setDescription(`已在<#${msg.channel.id}>封鎖一則訊息，傳送者<@${msg.author.id}>\n違規內容：傳送疑似Token訊息`)
-      .setColor([255,0,0])
-    chn.send({content:`<@${msg.author.id}>，Discord之規定不允許傳送Token，如有任何問題請詢問管理員`,embeds:[ebd]});
-    msg.delete();
-    return;
-  }else if(/https:\/\/discord.gg\/.*/gm.test(msg.content)&&msg.member.id!='606668363531288577'){
-    let chn = await msg.guild.channels.fetch('1020521950449246268');
-    msg.member.timeout(1000*60,'傳送連結');
-    let ebd = new Discord.MessageEmbed()
-      .setTitle('警告紀錄')
-      .setDescription(`已在<#${msg.channel.id}>封鎖一則訊息，傳送者<@${msg.author.id}>\n違規內容：傳送DC群組邀請連結`)
-      .setColor([255,0,0])
-   chn.send({content:`<@${msg.author.id}>，本群之規定不允許傳送DC邀請連結，如有任何問題請詢問管理員`,embeds:[ebd]});
-    msg.delete();
-    return;
   }
   await exp(msg,msg.author.id)
         .then(async a=>{
