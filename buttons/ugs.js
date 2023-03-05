1fa0e1ae1a7a591be86af0cb47b3d2970a3380a5
tobycoding0501 <tobycoding0501@gmail.com>
2023-03-05 15:03:27 +0800

    button

--- buttons/ugs.js
+++ buttons/ugs.js
@@ -1,5 +1,5 @@
 const { ActionRowBuilder, ButtonBuilder, ButtonStyle, EmbedBuilder } = require('discord.js');
-const customId = "rename"
+const customId = "ugs"
 const rpg_plugin = require('../plugins/rpg_plugin');
 const agree = require("./ugs_agree")
 const deny = require("./ugs_deny")
