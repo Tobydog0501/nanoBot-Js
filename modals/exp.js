2b41af7030599a84313979a9f085f9133016d57f
tobycoding0501 <tobycoding0501@gmail.com>
2023-04-10 23:22:05 +0800

    bugs

--- modals/exp.js
+++ modals/exp.js
@@ -18,12 +18,12 @@ module.exports = {
             )
         ),
 
-    async execute(inter,bot,Discord,userId){
+    async execute(modal,bot,Discord,userId){
         await modal.deferReply({ephemeral:true})
         var ret = await plu.adminExpSet(userId,modal.fields.getTextInputValue('exp-input'))
         await modal.guild.channels.fetch('993330070301180014')
           .then(async chn=>{
-            let ebd = new Discord.MessageEmbed()
+            let ebd = new EmbedBuilder()
               .setTitle('經驗值設置')
               .setDescription(`${modal.member}變更了<@${userId}>的經驗值`)
               .setFields([
@@ -31,7 +31,7 @@ module.exports = {
                 {name:'更動後',value:`等級：${ret['after']['lv']}\n經驗：${ret['after']['exp']}\n總經驗值：${ret['after']['totalExp']}`,inline:true}
               ])
               .setFooter({iconURL:modal.user.avatarURL(),text:`Changes commited by ${modal.user.tag}`})
-              .setColor('RANDOM')
+              .setColor('Random')
             await chn.send({embeds:[ebd]})
           })
         await modal.editReply({content:`Finished.\n已設置該使用者${modal.fields.getTextInputValue('exp-input')}經驗`,ephemeral:true})
